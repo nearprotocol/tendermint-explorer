041b6513abcaec2475f648c1212eb61c40a2aecb
azban <me@azban.net>
2019-04-24 23:31:06 -0700

    confnig: add quotes around env variable overrides

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  RPC_ADDR: process.env.RPC_ADDR || '"http://localhost:26657"',
-  WS_ADDR: process.env.WS_ADDR || '"ws://localhost:26657"'
+  RPC_ADDR: process.env.RPC_ADDR ? `"${process.env.RPC_ADDR}"` : '"http://localhost:26657"',
+  WS_ADDR: process.env.WS_ADDR ? `"${process.env.WS_ADDR}"` : '"ws://localhost:26657"',
+
 }
